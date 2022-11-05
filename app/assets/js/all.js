827aaa44ec7d07e22af835246075c6d4053fea26
Amber <melody19941231@gmail.com>
2022-11-05 14:24:21 +0800

    活動內容 & 賽程

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,3 +2,29 @@ AOS.init();
 gsap.registerPlugin(ScrollTrigger, Text);
 
 
+const swiper = new Swiper('.swiper', {
+    loop: false,
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 40
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        992: {
+          slidesPerView: 4,
+          spaceBetween: 40
+        }
+      },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+  });
